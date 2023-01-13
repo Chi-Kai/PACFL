1293ec1e4e046d03d0c4e27cf742122a3cca68cc
Chi-Kai <chikai0307@gmail.com>
2023-01-13 17:43:32 +0800

    update Fri Jan 13 05:43:32 PM CST 2023

--- scripts/cfl.sh
+++ scripts/cfl.sh
@@ -6,21 +6,21 @@ do
     fi 
     
     python ../main_cfl.py --trial=$trial \
-    --rounds=200 \
+    --rounds=20 \
     --num_users=100 \
     --frac=0.1 \
     --local_ep=10 \
     --local_bs=10 \
     --lr=0.01 \
     --momentum=0.5 \
-    --model=resnet9 \
-    --dataset=cifar100 \
+    --model=simple-cnn\
+    --dataset=cifar10 \
     --datadir='../../data/' \
     --logdir='../../logs/' \
     --savedir='../save_results/' \
     --partition='noniid-#label20' \
     --alg='cfl' \
-    --beta=0.1 \
+    --beta=0.5 \
     --local_view \
     --noise=0 \
     --cluster_alpha=0.3 \
